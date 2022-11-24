3656a9f9c814f8c7c42037ce6709b052b82ec6f1
“gechSV” <91822056+gechSV@users.noreply.github.com>
2022-11-24 19:04:09 +0900

    upd

--- app/src/main/java/ScheduleManagement/AndroidApp/ActivityAddScheduleItem.java
+++ app/src/main/java/ScheduleManagement/AndroidApp/ActivityAddScheduleItem.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ActivityAddScheduleItem extends AppCompatActivity {
     // _ET_EventName - текстовое поле для ввода названия события
     EditText _ET_EventName;
+    LinearLayout _LayoutColorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,14 @@ public class ActivityAddScheduleItem extends AppCompatActivity {
         setContentView(R.layout.activity_add_schedule_item);
 
         _ET_EventName = (EditText)findViewById(R.id.editTextEventName);
-
+        _LayoutColorButton = (LinearLayout)findViewById(R.id.ButtonLayout);
 
     }
 
+    /**
+     * Добавление кнопок выбора цвета
+     */
+    protected void CreateColorButton() {
+
+    }
 }
